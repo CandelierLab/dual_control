7790fb8002b4780a62ebf9ac840778d27e3473b8
bgallois <benjamin@gallois.cc>
2021-01-30 18:11:55 +0100

    [add] add fullscreen mode

--- Dual_GUI/Dual.cpp
+++ Dual_GUI/Dual.cpp
@@ -2,7 +2,7 @@
 #include "ui_Dual.h"
 
 Dual::Dual(QVector<QRect> screens, QString path, int id, QWidget *parent) : Screen(screens), path(path), guiid(id), QMainWindow(parent), ui(new Ui::Dual) {
- // this->setWindowState(Qt::WindowFullScreen);
+  this->setWindowState(Qt::WindowFullScreen);
 
   // === DEFINITIONS =====================================================
 
@@ -33,6 +33,7 @@ Dual::Dual(QVector<QRect> screens, QString path, int id, QWidget *parent) : Scre
   // === USER INTERFACE ==================================================
 
   ui->setupUi(this);
+  connect(ui->actionQuit, &QAction::triggered, this, &QMainWindow::close);
 
   ui->Image->setScaledContents(true);
 
